4f2fc75d4f847bc518a82f88739346dd8d3a3142
Aravind Gunda <aravindx.gunda@intel.com>
2020-02-19 21:58:01 -0800

    Using fscanf

---
+++ wordparse_mem.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+                         int main()
+{
+
+    time_t t1, t2;
+    int timing = 10;
+    char temp[50];
+    char word[15];
+    char word2[15];
+    int old = 0, new = 0;
+
+    t1 = time(NULL);
+    t2 = time(NULL);
+
+    while ((t2 - t1) < timing)
+    {
+        FILE *fp = fopen("/proc/meminfo", "r");
+        int line_num = 0;
+
+        if (fp == NULL)
+        {
+            printf("Error opening the file\n");
+            exit(1);
+        }
+
+        while (fscanf(fp, "%s %s %*s", word, word2) == 2)
+        {
+            if (line_num == 1)
+            { //Change this line number to get appropriate data
+                new = atoi(word2);
+                printf("%s %d, delta : %d\n", word, new, new - old);
+                old = new;
+                break;
+            }
+            line_num++;
+        }
+
+        t2 = time(NULL);
+        fclose(fp);
+        sleep(1);
+    }
+    return 0;
+}
